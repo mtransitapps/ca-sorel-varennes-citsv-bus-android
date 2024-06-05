30caa40d139c4468c8cef7340ef50decfcbdc7fb
Mathieu Méa <mathieu.mea@gmail.com>
2024-06-04 20:22:19 -0400

    Migrate to new flatten git repo

---
+++ agency-parser/src/main/java/org/mtransit/parser/ca_sorel_varennes_citsv_bus/SorelVarennesCITSVBusAgencyTools.java
@@ -0,0 +1,192 @@
+package org.mtransit.parser.ca_sorel_varennes_citsv_bus;
+
+import static org.mtransit.commons.Constants.SPACE_;
+import static org.mtransit.commons.RegexUtils.DIGITS;
+import static org.mtransit.commons.StringUtils.EMPTY;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.mtransit.commons.CharUtils;
+import org.mtransit.commons.CleanUtils;
+import org.mtransit.commons.RegexUtils;
+import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.MTLog;
+import org.mtransit.parser.gtfs.data.GRoute;
+import org.mtransit.parser.gtfs.data.GStop;
+import org.mtransit.parser.mt.data.MAgency;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// https://exo.quebec/en/about/open-data
+public class SorelVarennesCITSVBusAgencyTools extends DefaultAgencyTools {
+
+	public static void main(@NotNull String[] args) {
+		new SorelVarennesCITSVBusAgencyTools().start(args);
+	}
+
+	@Nullable
+	@Override
+	public List<Locale> getSupportedLanguages() {
+		return LANG_FR;
+	}
+
+	@Override
+	public boolean defaultExcludeEnabled() {
+		return true;
+	}
+
+	@NotNull
+	@Override
+	public String getAgencyName() {
+		return "exo Sorel-Varennes";
+	}
+
+	@NotNull
+	@Override
+	public Integer getAgencyRouteType() {
+		return MAgency.ROUTE_TYPE_BUS;
+	}
+
+	@Override
+	public boolean defaultRouteIdEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean useRouteShortNameForRouteId() {
+		return true;
+	}
+
+	@NotNull
+	@Override
+	public String getRouteShortName(@NotNull GRoute gRoute) {
+		return gRoute.getRouteShortName(); // used by GTFS-RT
+	}
+
+	@Override
+	public boolean defaultRouteLongNameEnabled() {
+		return true;
+	}
+
+	@NotNull
+	@Override
+	public String cleanRouteLongName(@NotNull String routeLongName) {
+		routeLongName = CleanUtils.SAINT.matcher(routeLongName).replaceAll(CleanUtils.SAINT_REPLACEMENT);
+		return CleanUtils.cleanLabel(routeLongName);
+	}
+
+	@Override
+	public boolean defaultAgencyColorEnabled() {
+		return true;
+	}
+
+	private static final Pattern SERVICE_LOCAL = Pattern.compile("(service local)", Pattern.CASE_INSENSITIVE);
+
+	private static final Pattern EXPRESS_ = CleanUtils.cleanWordsFR("express");
+
+	private static final Pattern _DASH_ = Pattern.compile("( - )");
+	private static final String _DASH_REPLACEMENT = "<>"; // form<>to
+
+	@NotNull
+	@Override
+	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
+		tripHeadsign = CleanUtils.keepToFR(tripHeadsign);
+		tripHeadsign = _DASH_.matcher(tripHeadsign).replaceAll(_DASH_REPLACEMENT); // from - to => form<>to
+		tripHeadsign = EXPRESS_.matcher(tripHeadsign).replaceAll(EMPTY);
+		tripHeadsign = DEVANT_.matcher(tripHeadsign).replaceAll(EMPTY);
+		tripHeadsign = SERVICE_LOCAL.matcher(tripHeadsign).replaceAll(EMPTY);
+		tripHeadsign = CleanUtils.cleanBounds(Locale.FRENCH, tripHeadsign);
+		tripHeadsign = CleanUtils.cleanStreetTypesFRCA(tripHeadsign);
+		return CleanUtils.cleanLabelFR(tripHeadsign);
+	}
+
+	@Override
+	public boolean directionFinderEnabled() {
+		return true;
+	}
+
+	private static final Pattern START_WITH_FACE_A = Pattern.compile("^(face à )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.CANON_EQ);
+	private static final Pattern START_WITH_FACE_AU = Pattern.compile("^(face au )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+	private static final Pattern START_WITH_FACE = Pattern.compile("^(face )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+
+	private static final Pattern SPACE_FACE_A = Pattern.compile("( face à )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.CANON_EQ);
+	private static final Pattern SPACE_WITH_FACE_AU = Pattern.compile("( face au )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+	private static final Pattern SPACE_WITH_FACE = Pattern.compile("( face )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+
+	private static final Pattern[] START_WITH_FACES = new Pattern[]{START_WITH_FACE_A, START_WITH_FACE_AU, START_WITH_FACE};
+
+	private static final Pattern[] SPACE_FACES = new Pattern[]{SPACE_FACE_A, SPACE_WITH_FACE_AU, SPACE_WITH_FACE};
+
+	private static final Pattern DEVANT_ = CleanUtils.cleanWordsFR("devant");
+
+	@NotNull
+	@Override
+	public String cleanStopName(@NotNull String gStopName) {
+		gStopName = _DASH_.matcher(gStopName).replaceAll(SPACE_);
+		gStopName = DEVANT_.matcher(gStopName).replaceAll(EMPTY);
+		gStopName = RegexUtils.replaceAllNN(gStopName, START_WITH_FACES, CleanUtils.SPACE);
+		gStopName = RegexUtils.replaceAllNN(gStopName, SPACE_FACES, CleanUtils.SPACE);
+		gStopName = CleanUtils.cleanBounds(getFirstLanguageNN(), gStopName);
+		gStopName = CleanUtils.cleanStreetTypesFRCA(gStopName);
+		return CleanUtils.cleanLabelFR(gStopName);
+	}
+
+	private static final String ZERO = "0";
+
+	@NotNull
+	@Override
+	public String getStopCode(@NotNull GStop gStop) {
+		if (ZERO.equals(gStop.getStopCode())) {
+			return EMPTY;
+		}
+		//noinspection deprecation
+		return gStop.getStopId(); // used by GTFS-RT
+	}
+
+	@Override
+	public int getStopId(@NotNull GStop gStop) {
+		String stopCode = getStopCode(gStop);
+		if (stopCode.length() > 0 && CharUtils.isDigitsOnly(stopCode)) {
+			return Integer.parseInt(stopCode); // using stop code as stop ID
+		}
+		//noinspection deprecation
+		final String stopId1 = gStop.getStopId();
+		final Matcher matcher = DIGITS.matcher(stopId1);
+		if (matcher.find()) {
+			final int digits = Integer.parseInt(matcher.group());
+			int stopId;
+			if (stopId1.startsWith("BOU")) {
+				stopId = 100_000;
+			} else if (stopId1.startsWith("LON")) {
+				stopId = 200_000;
+			} else if (stopId1.startsWith("VAR")) {
+				stopId = 300_000;
+			} else if (stopId1.startsWith("SAM")) {
+				stopId = 400_000;
+			} else if (stopId1.startsWith("VCH")) {
+				stopId = 500_000;
+			} else if (stopId1.startsWith("CON")) {
+				stopId = 600_000;
+			} else {
+				throw new MTLog.Fatal("Stop doesn't have an ID (start with) %s!", gStop);
+			}
+			if (stopId1.endsWith("A")) {
+				stopId += 1_000;
+			} else if (stopId1.endsWith("B")) {
+				stopId += 2_000;
+			} else if (stopId1.endsWith("C")) {
+				stopId += 3_000;
+			} else if (stopId1.endsWith("D")) {
+				stopId += 4_000;
+			} else {
+				throw new MTLog.Fatal("Stop doesn't have an ID (end with) %s!", gStop);
+			}
+			return stopId + digits;
+		} else {
+			throw new MTLog.Fatal("Unexpected stop ID %s!", gStop);
+		}
+	}
+}
